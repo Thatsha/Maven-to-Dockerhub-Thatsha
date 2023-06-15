858bc8bbbef5591e8f42bd149b83f33040b78438
Thatsha Yathu <thatshayathu36@gmail.com>
2023-06-15 18:11:57 +0100

    maven-git-jenkins-dockerhub

---
+++ src/main/java/maven_git_jenkins_dockerhub/DataTypes.java
@@ -0,0 +1,34 @@
+package maven_git_jenkins_dockerhub;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		byte a=12;
+		System.out.println(a);
+		
+		short s=32758;
+		System.out.println(s);
+		
+		int i=2147483468;
+		System.out.println(i);
+		
+		long l=922337301;
+		System.out.println(l);
+		
+		float f= 32.5555555367777484641255f;
+		System.out.println(f);
+		
+		double d= 32.55555536777748461255;
+		System.out.println(d);
+		
+		boolean b= true;
+		System.out.println(b);
+		
+		char c='X';
+		System.out.println(c);
+		
+		String st= "Happy Day 18/08/2022**";
+		System.out.println(st);			
+	}
+	}
+
+
